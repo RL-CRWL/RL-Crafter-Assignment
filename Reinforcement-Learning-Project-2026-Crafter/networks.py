2dd3b6c41e76db606c4e209ba8889c77706e6367
LiamB22 <LMBrady5454@gmail.com>
2025-10-22 02:28:03 +0200

    fixed kernel sizeing

--- Reinforcement-Learning-Project-2026-Crafter/networks.py
+++ Reinforcement-Learning-Project-2026-Crafter/networks.py
@@ -9,13 +9,13 @@ class ModifiedCNN(BaseFeaturesExtractor):
         self.cnn = nn.Sequential(
             nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=4),
             nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.Conv2d(32, 64, kernel_size=2, stride=2),
             nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=3, stride=2),
+            nn.Conv2d(64, 128, kernel_size=2, stride=2),
             nn.ReLU(),
-            nn.Conv2d(128, 128, kernel_size=3, stride=1),
+            nn.Conv2d(128, 128, kernel_size=2, stride=1),
             nn.ReLU(),
-            nn.Conv2d(128, 256, kernel_size=3, stride=1),
+            nn.Conv2d(128, 128, kernel_size=2, stride=1),
             nn.ReLU(),
             nn.Flatten()
         )
